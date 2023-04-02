3e478d54fca258c60d997cecacc651e7143c11af
caiquemiranda <caiquemirandas@gmail.com>
2023-04-02 06:03:28 -0300

    Working with files

---
+++ scripts/Working_with_dict_example_1.py
@@ -0,0 +1,11 @@
+
+result = {}
+
+with open('data/sh_ip_int_br.txt') as f:
+    for line in f:
+        line = line.split()
+        if line and line[1][0].isdigit():
+            interface, address, *other = line
+            result[interface] = address
+
+print(result)
